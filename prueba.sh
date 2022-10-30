82ac784fd47f3903b6df138836dd37bd6bd880e4
SkyRight018 <116565072+SkyRight018@users.noreply.github.com>
2022-10-30 12:27:09 -0300

    Add files via upload

---
+++ prueba.sh
@@ -0,0 +1,49 @@
+clear
+setterm -foreground green
+
+echo '.____                    .__                                .__         
+|    |     ____    ____  |__|  ____    ____  _____  _______ |__|  ____  
+|    |   _/ __ \  / ___\ |  | /  _ \  /    \ \__  \ \_  __ \|  | /  _ \ 
+|    |___\  ___/ / /_/  >|  |(  <_> )|   |  \ / __ \_|  | \/|  |(  <_> )
+|_______ \\___  >\___  / |__| \____/ |___|  /(____  /|__|   |__| \____/ 
+        \/    \//_____/                   \/      \/                    '
+
+setterm -foreground red
+
+echo "Elije Una Opción"
+
+setterm -foreground green
+
+echo "[1] Hackear WhatsApp"
+echo "[2] Crear Binarios Para Android"
+echo "[3] Crear Binarios Para Iphone"
+echo "[4] Resetear Prede De buzon +1"
+
+read $op
+
+echo -ne ' Cargando programa..    (6%)\r'
+sleep 2
+echo -ne ' Cargando programa...  (18%)\r'
+sleep 2
+echo -ne ' Cargando programa.... (37%)\r'
+sleep 2
+echo -ne ' Cargando programa...  (40%)\r'
+sleep 2
+echo -ne ' Cargando programa..   (62%)\r'
+sleep 2
+echo -ne ' Cargando programa.    (89%)\r'
+sleep 2
+echo -ne ' Cargando programa    (100%)\r'
+
+clear
+echo "!Cargado Con Exito¡"
+
+mkdir porno y cp de niños albinos.apk
+mkdir setzo de vacas.sh
+mkdir fuiste troleado pendejo legiovirgo.txt
+mkdir porno.py
+mkdir chupame el pene.php
+mkdir agarrarme la polla.png
+mkdir los +687 me pelan toda la verga.html
+mkdir porno y cp.apk
+mkdir porno de gays.png
